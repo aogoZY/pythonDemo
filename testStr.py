7fa02493aaff370f31015869b54110bb2a9238f6
周阳 <zhouyang@zhouyangdeMacBook-Pro.local>
2019-09-03 00:20:35 +0800

    test str

---
+++ testStr.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+# 格式化字符串
+# 方法一 using %
+name = 'aogo'
+age = 18
+print ("%s is a %d year old boy" % (name, age))   # aogo is a 18 year old boy
+
+# 方法2 format
+print ('{0} likes playing {1}'.format('aogo', 'bedmiton'))  # aogo likes playing bedmiton
+
